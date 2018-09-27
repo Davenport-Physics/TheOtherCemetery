e78c4c53386fdaa257573fc9e47ca98f16b16eb1
Emma Davenport <Davenport.Physics@gmail.com>
2018-09-27 15:21:17 -0500

    Funeral Scene World Defined
    Former-commit-id: a4c74ee0df6fd57dda66d63ed7be9cbbacdd0608

--- src/levels/day0/level.lua
+++ src/levels/day0/level.lua
@@ -1,11 +1,28 @@
+
+local WorldClass   = require("src/world/world")
+local FuneralScene = require("src/levels/day0/scenes/FuneralHome")
+
+local FuneralSceneMap        = FuneralScene.GetMap()
+local FuneralSceneChars      = FuneralScene.GetCharacters()
+local FuneralScenePlayerChar = FuneralScene.GetPlayerCharacter()
+local FuneralSceneEntities   = FuneralScene.GetEntities()
+
+local FuneralWorld = WorldClass:new(FuneralSceneMap, FuneralSceneChars, FuneralScenePlayerChar, FuneralSceneEntities)
+
 function Draw()
+
+    FuneralWorld:Draw()
 
 end
 
 function Update()
 
+    FuneralWorld:Update()
+
 end
 
-function Handle_Input()
+function HandleInput()
+
+    FuneralWorld:HandleInput()
 
 end
--- src/levels/day0/scenes/FuneralHome.lua
+++ src/levels/day0/scenes/FuneralHome.lua
@@ -1,4 +1,5 @@
-local MapClass = require("src/map/map")
+local MapClass    = require("src/map/map")
+local EntityClass = require("src/entity/entity")
 
 
 local FuneralHomeFloorDataLocation = "tiles/autumn-platformer-tileset/png/elements/"
@@ -13,3 +14,39 @@ local FuneralHomeFloorData =
 }
 
 local FuneralHomeFloorMap = MapClass:new(FuneralHomeFloorData, FuneralHomeFloorDataLocation)
+
+local SeatingPlatformImage = love.graphics.newImage("tiles/zombie-tileset/Objects/PNG/objects_house_0042_Layer-42.png")
+local SeatingPlatformEntites =
+{
+
+    {EntityClass:newWithImage(SeatingPlatformImage, 100, 200, false)},
+
+
+}
+
+local CharactersInScene = {}
+local PlayerCharacter = {}
+
+function GetMap()
+
+    return FuneralHomeFloorMap
+
+end
+
+function GetEntities()
+
+    return SeatingPlatformEntites
+
+end
+
+function GetCharacters()
+
+    return CharactersInScene
+
+end
+
+function GetPlayerCharacter()
+
+    return PlayerCharacter
+
+end
