6a2ab1d467a63e6551401384a3bf7f4f262cbc26
Emma Davenport <davenport.physics@gmail.com>
2018-09-29 13:42:57 -0500

    Slight refactor
    Former-commit-id: b547f918cb18edf67a00bd533eea91c450dd4e71

--- main.lua
+++ main.lua
@@ -32,59 +32,83 @@ local CONTEXT_FUNCTIONS =
 local CURRENT_CONTEXT = CONTEXT_INDEX.STARTING_WINDOW
 
 local function InStartMenu()
+
     love.audio.stop()
     StartMenuMusic_Start()
     CURRENT_CONTEXT = CONTEXT_INDEX.STARTING_WINDOW
+
 end
 
 local function StartNewGame()
+
     love.audio.stop()
     CURRENT_CONTEXT = CONTEXT_INDEX.NEW_GAME
+
 end
 
 local function LoadGame()
+
     CURRENT_CONTEXT = CONTEXT_INDEX.LOAD_GAME
+
 end
 
 local function Game()
+
     love.audio.stop()
     CURRENT_CONTEXT = CONTEXT_INDEX.GAME
+
 end
 
 local function Credits()
+
     love.audio.stop()
     ResetCreditsPositionText()
     StartCreditMusic()
     CURRENT_CONTEXT = CONTEXT_INDEX.CREDITS
+
 end
 
 local function Options()
+
     CURRENT_CONTEXT = CONTEXT_INDEX.OPTIONS
+
 end
 
 local function Quit()
+
     love.event.quit()
+
 end
 
 function love.load()
+
     StartMenuMusic_Start()
     InitializeStartMenu_CallBackFunctions(StartNewGame, LoadGame, Options, Credits, Quit)
     InitializeCredits_CallBackFunctions(InStartMenu)
+
 end
 
 local function HandleInput()
+
     CONTEXT_FUNCTIONS[CURRENT_CONTEXT].Input()
+
 end
 
 function love.update()
+
     HandleInput()
+
 end
 
 function love.draw()
+
     CONTEXT_FUNCTIONS[CURRENT_CONTEXT].Draw()
     love.graphics.print("Current FPS: "..tostring(love.timer.getFPS( )), 500, 10)
+
 end
 
 function love.quit()
+
   print("Thanks for playing! Come back soon!")
+
 end
