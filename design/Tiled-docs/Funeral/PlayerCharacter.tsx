0ffeb0a25d09e432d0a6137b955e604c3dc1e38f
Emma Davenport <davenport.physics@gmail.com>
2018-10-01 19:32:06 -0500

    Various Changes
    Former-commit-id: 94ba3cef29487e26b22a9d8bfc81d592aa1cf7a7

---
+++ design/Tiled-docs/Funeral/PlayerCharacter.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="PlayerCharacter" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="9" columns="3">
+ <image source="../../../tiles/Characters/Females/F_01.png" width="64" height="51"/>
+</tileset>
