7a736dca7700a5163ca177f6da93ef4c61709860
Emma Davenport <Davenport.Physics@gmail.com>
2018-10-10 13:14:20 -0500

    Added Door class
    Former-commit-id: eb4d061f961bef2852d5c4266756838116f1013c

---
+++ src/entity/door.lua
@@ -0,0 +1,30 @@
+local Door = {}
+Door.__index = Door
+
+local CollionClass = require("src/collision/collision")
+
+function Door:new(x_pos, y_pos, width, height, transition_to_scene)
+
+    local obj = {}
+    setmetatable(obj, Door)
+    obj.x_pos  = x_pos
+    obj.y_pos  = y_pos
+    obj.width  = width
+    obj.height = height
+    obj.transition_to_scene = transition_to_scene
+    obj.collision_obj       = CollionClass:new(x_pos, y_pos, width, height, "door")
+
+    return obj
+
+end
+
+function Door:CheckForCollision(x_pos, y_pos)
+
+    if self.collision_obj:CheckForCollision(x_pos, y_pos) then
+        return transition_to_scene
+    end
+    return false
+
+end
+
+return Door
