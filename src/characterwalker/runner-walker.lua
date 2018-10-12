2e0461f7754281d9f0cdd9a9ca2bd492f4ef6609
Emma Davenport <Davenport.Physics@gmail.com>
2018-10-12 18:10:32 -0500

    Added first runner walker code that is finished
    Former-commit-id: d0709d1b8d857e2c54b5f5a3040045be915c1431

--- src/characterwalker/runner-walker.lua
+++ src/characterwalker/runner-walker.lua
@@ -7,37 +7,67 @@ function RunnerWalker:new(char, walker_instructions)
     setmetatable(obj, RunnerWalker)
     obj.char = char
     obj.walker_instructions = walker_instructions
+    obj.walk_direction      = walker_instructions.WalkDirection
+    obj.player              = walker_instructions.player
 
     return obj
 
 end
 
 function RunnerWalker:AnnimateMove()
-    if self.walker_instructions.WalkDirection == "WalkDown" then
+    if self.walk_direction == "WalkDown" then
         self.char:WalkDown(false)
         self.char:GlideDown()
-    elseif self.walker_instructions.WalkDirection == "WalkUp" then
+    elseif self.walk_direction == "WalkUp" then
         self.char:WalkUp(false)
         self.char:GlideUp()
-    elseif self.walker_instructions.WalkDirection == "WalkLeft" then
+    elseif self.walk_direction == "WalkLeft" then
         self.char:WalkLeft(false)
         self.char:GlideLeft()
-    elseif self.walker_instructions.WalkDirection == "WalkRight" then
+    elseif self.walk_direction == "WalkRight" then
         self.char:WalkRight(false)
         self.char:GlideRight()
     end
 end
 
+function RunnerWalker:CalculateHorizontalGlide()
+
+    local dx = self.char.x_pos - self.player.x_pos
+    if math.abs(dx) >= 2 then
+        dx = math.floor((dx/math.abs(dx)) * 2)
+    end
+    if math.abs(dx) >= .05 then
+        self.char:GlideRight(dx)
+    end
+
+end
+
+function RunnerWalker:CalculateVerticalGlide()
+
+    local dy = self.char.y_pos - self.player.y_pos
+    if math.abs(dy) >= 2 then
+        dy = math.floor((dy/math.abs(dy)) * 2)
+    end
+    if math.abs(dy) >= .05 then
+        self.char:GlideUp(dy)
+    end
+
+end
+
 function RunnerWalker:CalculateSidestep()
 
-    
+    if self.walk_direction == "WalkUp" or self.walk_direction == "WalkDown" then
+        self:CalculateHorizontalGlide()
+    elseif self.walk_direction == "WalkRight" or self.walk_direction == "WalkLeft" then
+        self:CalculateVerticalGlide()
+    end
 
 end
 
 function RunnerWalker:Move()
 
     self:AnnimateMove()
-    self:
+    self:CalculateSidestep()
 
 end
 
