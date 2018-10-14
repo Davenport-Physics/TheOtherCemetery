0457230cb22a4f383438aedda621b71275137c13
Emma Davenport <Davenport.Physics@gmail.com>
2018-10-14 09:49:41 -0500

    Added new tilset
    Former-commit-id: caa098c63db39145886ceb5fc3b039069f4dbe77

---
+++ design/Tiled-docs/henry-bedroom/roguelikeIndoor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="roguelikeIndoor" tilewidth="16" tileheight="16" spacing="1" tilecount="468" columns="26">
+ <image source="../../../tiles/Roguelike-Indoor-pack/Spritesheet/roguelikeIndoor_transparent.png" width="457" height="305"/>
+</tileset>
