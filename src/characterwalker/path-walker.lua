9deac14a47058b82bca527dc96f77f279dd360ea
Emma Davenport <Davenport.Physics@gmail.com>
2018-10-17 12:18:29 -0500

    Added functions and member vars for path walker
    Former-commit-id: bcfdc248a8beb8b93e7edb3584144ae34aee863d

--- src/characterwalker/path-walker.lua
+++ src/characterwalker/path-walker.lua
@@ -7,16 +7,35 @@ function PathWalker:new(char, walker_instructions)
     setmetatable(obj, PathWalker)
     obj.char = char
     obj.walker_instructions = walker_instructions
-    obj.path = walker_instructions.path
-    obj.is_done_walking = false
+    obj:Init()
 
     return obj
 
 end
 
+function PathWalker:Init()
+
+    self.is_done_walking = false
+    self.path       = self.walker_instructions.Path
+    self.loop       = self.walker_instructions.Loop or false
+    self.loop_amt   = self.walker_instructions.TimesToLoop or 0
+
+    self.point_made = {}
+    for i = 1, #self.path do
+        self.point_made[i] = false
+    end
+
+end
+
+function PathWalker:CheckForPointsMade()
+
+
+
+end
+
 function PathWalker:Update()
 
-
+    self:CheckForPointsMade()
 
 end
 
