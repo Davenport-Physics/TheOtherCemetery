853f856f0316ffc16800111a9c3ebc0efee14a49
Emma Davenport <Davenport.Physics@gmail.com>
2018-10-22 12:33:32 -0500

    Various changes
    Former-commit-id: dfef1655e1a7d6c8d6b56ea943358baa9c2b7184

---
+++ design/mindmaps/Advertising.mm
@@ -0,0 +1,82 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Advertising" FOLDED="false" ID="ID_991410594" CREATED="1540152199829" MODIFIED="1540152579426" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Twitter" POSITION="right" ID="ID_1567712735" CREATED="1540152301097" MODIFIED="1540152579423" HGAP_QUANTITY="13.250000022351742 pt" VSHIFT_QUANTITY="-39.74999881535772 pt">
+<edge COLOR="#ff0000"/>
+<node TEXT="Icon" ID="ID_1674088964" CREATED="1540152344974" MODIFIED="1540152370989"/>
+<node TEXT="Banner" ID="ID_487046116" CREATED="1540152379879" MODIFIED="1540152383383"/>
+<node TEXT="Start Tweeting" ID="ID_121904627" CREATED="1540152392350" MODIFIED="1540152417158"/>
+</node>
+<node TEXT="Discord Channel" POSITION="right" ID="ID_1101636725" CREATED="1540152450466" MODIFIED="1540152575857" HGAP_QUANTITY="19.99999982118607 pt" VSHIFT_QUANTITY="47.24999859184031 pt">
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="Youtube" POSITION="right" ID="ID_809098827" CREATED="1540152495032" MODIFIED="1540152574272" HGAP_QUANTITY="11.000000089406964 pt" VSHIFT_QUANTITY="42.74999872595075 pt">
+<edge COLOR="#00ff00"/>
+<node TEXT="Trailer" ID="ID_1536918658" CREATED="1540152511199" MODIFIED="1540152517182"/>
+<node TEXT="Vlog" ID="ID_1677880305" CREATED="1540152519481" MODIFIED="1540152565661"/>
+</node>
+</node>
+</map>
