aa9e437780b9222b954fed8c154e8d9de4bf0840
Emma Davenport <Davenport.Physics@gmail.com>
2018-10-27 15:34:33 -0500

    Added new shared function IsNear
    Former-commit-id: 1ca6c47879e557cde28b49a057c92232e7b6de90

--- src/shared/shared.lua
+++ src/shared/shared.lua
@@ -7,4 +7,14 @@ function Shared.IsBetweenRange(value, low, high)
 
 end
 
+function Shared.IsNear(x_1, y_1, x_2, y_2, distance_threshold)
+
+    if math.sqrt((x_1 - x_2)^2 + (y_1 - y_2)^2) <= distance_threshold then
+        return true
+    end
+
+    return false
+
+end
+
 return Shared
