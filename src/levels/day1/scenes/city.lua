55e4fef0f88c9bcf4fc946359f3719078256d98c
Emma Davenport <Davenport.Physics@gmail.com>
2018-10-30 19:57:21 -0500

    Added a little more text
    Former-commit-id: 87d28b91ef4a6ec48ebc80da3bf7f0ef2cfdb6a4

--- src/levels/day1/scenes/city.lua
+++ src/levels/day1/scenes/city.lua
@@ -148,22 +148,24 @@ end
 
 local DoNotGoInText =
 {
-    {x_pos = 23*16, y_pos = 59*16, text = TextBubbleClass:new(Henry, "pics/share/text/TextBubbleSpeaking.png", "Locked...")},
-    {x_pos = 29*16, y_pos = 59*16, text = TextBubbleClass:new(Henry, "pics/share/text/TextBubbleSpeaking.png", "Locked...")},
-    {x_pos = 35*16, y_pos = 59*16, text = TextBubbleClass:new(Henry, "pics/share/text/TextBubbleSpeaking.png", "Locked...")},
-    {x_pos = 41*16, y_pos = 59*16, text = TextBubbleClass:new(Henry, "pics/share/text/TextBubbleSpeaking.png", "Locked...")},
-    {x_pos = 16*16, y_pos = 40*16, text = TextBubbleClass:new(Henry, "pics/share/text/TextBubbleSpeaking.png", "I shouldn't go here")},
-    {x_pos = 25*16, y_pos = 41*16, text = TextBubbleClass:new(Henry, "pics/share/text/TextBubbleSpeaking.png", "I should go to school")},
-    {x_pos = 53*16, y_pos = 37*16, text = TextBubbleClass:new(Henry, "pics/share/text/TextBubbleSpeaking.png", "...")},
-    {x_pos = 42*16, y_pos = 40*16, text = TextBubbleClass:new(Henry, "pics/share/text/TextBubbleSpeaking.png", "I should go to school")},
-    {x_pos = 36*16, y_pos = 41*16, text = TextBubbleClass:new(Henry, "pics/share/text/TextBubbleSpeaking.png", "I shouldn't go here")},
+    {x_pos = 23*16, y_pos = 59*16, text = TextBubbleClass:new(Henry, "pics/share/text/TextBubble.png", "Locked...")},
+    {x_pos = 29*16, y_pos = 59*16, text = TextBubbleClass:new(Henry, "pics/share/text/TextBubble.png", "Locked...")},
+    {x_pos = 35*16, y_pos = 59*16, text = TextBubbleClass:new(Henry, "pics/share/text/TextBubble.png", "Locked...")},
+    {x_pos = 41*16, y_pos = 59*16, text = TextBubbleClass:new(Henry, "pics/share/text/TextBubble.png", "Locked...")},
+    {x_pos = 16*16, y_pos = 40*16, text = TextBubbleClass:new(Henry, "pics/share/text/TextBubble.png", "I shouldn't go here")},
+    {x_pos = 25*16, y_pos = 41*16, text = TextBubbleClass:new(Henry, "pics/share/text/TextBubble.png", "I should go to school")},
+    {x_pos = 53*16, y_pos = 37*16, text = TextBubbleClass:new(Henry, "pics/share/text/TextBubble.png", "...")},
+    {x_pos = 42*16, y_pos = 40*16, text = TextBubbleClass:new(Henry, "pics/share/text/TextBubble.png", "I should go to school")},
+    {x_pos = 36*16, y_pos = 41*16, text = TextBubbleClass:new(Henry, "pics/share/text/TextBubble.png", "I shouldn't go here")},
+    {x_pos = 67*16, y_pos = 8*16, text = TextBubbleClass:new( Henry, "pics/share/text/TextBubble.png", "I shouldn't go here")},
+    {x_pos = 74*16, y_pos = 6*16, text = TextBubbleClass:new( Henry, "pics/share/text/TextBubble.png", "Nothing here..."), range = 48},
 
 }
 local function DrawHenryStopTextBubblesIfPossible()
 
     for i = 1, #DoNotGoInText do
 
-        if Shared.IsNear(Henry.x_pos, Henry.y_pos, DoNotGoInText[i].x_pos, DoNotGoInText[i].y_pos, 17) then
+        if Shared.IsNear(Henry.x_pos, Henry.y_pos, DoNotGoInText[i].x_pos, DoNotGoInText[i].y_pos, DoNotGoInText[i].range or 17) then
             DoNotGoInText[i].text:Draw()
         end
 
