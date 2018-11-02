190a02ff42c073b464814fe408bae4c49a937439
Emma Davenport <Davenport.Physics@gmail.com>
2018-11-02 17:36:04 -0500

    Added specific turn feature
    Former-commit-id: d2a50a07e742b13334263ef75120ced57776f047

--- src/characterwalker/stationaryturn.lua
+++ src/characterwalker/stationaryturn.lua
@@ -9,13 +9,13 @@ function StationaryTurn:new(char, walker_instructions)
     obj.dt   = walker_instructions.DirectionDt
     obj.next_dir_change = love.timer.getTime() + obj.dt
     obj.current_dir     = walker_instructions.CurrentDirection
+    obj.specific_turns  = walker_instructions.SpecificTurns
 
     return obj
 
 end
 
-function StationaryTurn:DetermineNextDirection()
-
+function StationaryTurn:GenericDirection()
     if self.current_dir == "Down" then
         self.current_dir = "Left"
         self.char:WalkLeft()
@@ -28,6 +28,29 @@ function StationaryTurn:DetermineNextDirection()
     elseif self.current_dir == "Right" then
         self.current_dir = "Down"
         self.char:WalkDown()
+    end
+end
+
+function StationaryTurn:SpecificTurns()
+
+    for i = 1, #self.specific_turns do
+
+        if self.current_dir == self.specific_turns[i].dir then
+            self.current_dir = self.specific_turns[i].new_dir
+            self.specific_turns[i].func(self.char)
+            return
+        end
+
+    end
+
+end
+
+function StationaryTurn:DetermineNextDirection()
+
+    if self.specific_turns == nil then
+        self:GenericDirection()
+    else
+        self:SpecificTurns()
     end
 
 end
--- src/levels/day1/scenes/interiors/collector.lua
+++ src/levels/day1/scenes/interiors/collector.lua
@@ -17,7 +17,23 @@ local transition = false
 local Map        = TiledMapClass:new(require("src/levels/maps/interiors/collector/collector"))
 
 local Henry = CharacterClass:new("tiles/Characters/Males/M_08.png", 11*16, 12*16, 16, 17, 6, .05); Henry:WalkUp();
-local NPCs  = {}
+local NPCs  =
+{
+    CharacterClass:new("tiles/Characters/Females/F_08.png", 7*16, 5*16, 16, 17, 4, .05)
+}
+
+local TurnWalkerIntructions =
+{
+
+    DirectionDt = 2,
+    CurrentDirection = "Down",
+    SpecificTurns =
+    {{dir = "Right", func =  NPCs[1].WalkDown, new_dir = "Down"},
+    {dir = "Down" , func = NPCs[1].WalkRight, new_dir = "Right"}},
+
+}
+local WorkerTurner = WalkerClass:new(NPCs[1], "turn-walker", TurnWalkerIntructions)
+
 local World = WorldClass:new(Map, NPCs, Henry, Map:GetCollisionObjects())
 World:SetEntityToTrackForCamera(Henry)
 
@@ -25,6 +41,8 @@ local ExitDoor = DoorClass:new(10*16, 13*16, 3*16, 16, "src/levels/day1/scenes/c
 --local DeepDoor = DoorClass:new(12*16, 5*16, 16, 16, "")
 
 local Doors = DoorsHandler:new({ExitDoor}, Henry)
+
+local WorkerText = TextBubbleClass:new(NPCs[1], "pics/share/text/TextBubbleSpeaking.png", "Welcome!")
 
 local function UpdateDoorTransitions()
 
@@ -35,16 +53,32 @@ local function UpdateDoorTransitions()
 
 end
 
+local function UpdateWalker()
+
+    WorkerTurner:Update()
+
+end
+
+local function DrawWorkerTextIfPossible()
+
+    if Shared.IsNear(Henry.x_pos, Henry.y_pos, NPCs[1].x_pos, NPCs[1].y_pos, 48) then
+        WorkerText:Draw()
+    end
+
+end
+
 function Scene.Update()
 
     World:Update()
     UpdateDoorTransitions()
+    UpdateWalker()
 
 end
 
 function Scene.Draw()
 
     World:Draw()
+    DrawWorkerTextIfPossible()
 
 end
 
