c9768f44e879830b1547aeaee3d27d64558f2812
Emma Davenport <davenport.physics@gmail.com>
2018-11-03 17:59:22 -0500

    Fixed funeral home issue
    Former-commit-id: 88c88c4a4013e7fa31a4c9bb619131342e3bfc68

--- src/levels/day0/scenes/FuneralHome.lua
+++ src/levels/day0/scenes/FuneralHome.lua
@@ -37,8 +37,8 @@ local AnnaText =
 {
 
     TextBubbleClass:new(AnnaStationaryText, "pics/share/text/TextBoxes.png", "Moving here was supposed to be a \nnew change for my family."),
-    TextBubbleClass:new(AnnaStationaryText, "pics/share/text/TextBoxes.png", "However, I never anticipated that it \nwould mean this..."),
-    TextBubbleClass:new(AnnaStationaryText, "pics/share/text/TextBoxes.png", "I wanted to thank all of you for \nbeing here. Most of you don’t know \nDuncan,"),
+    TextBubbleClass:new(AnnaStationaryText, "pics/share/text/TextBoxes.png", "However, I never anticipated \nthat it would mean this..."),
+    TextBubbleClass:new(AnnaStationaryText, "pics/share/text/TextBoxes.png", "I wanted to thank all of you \nfor being here. Most of you \ndon’t know Duncan,"),
     TextBubbleClass:new(AnnaStationaryText, "pics/share/text/TextBoxes.png", "but I am sure he would have \nappreciated it."),
     TextBubbleClass:new(AnnaCharacter, "pics/share/text/TextBubbleSpeaking.png", "Thank you."),
 
