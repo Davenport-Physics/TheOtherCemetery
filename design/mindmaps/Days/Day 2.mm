5f16be2c8c0055abeed9b5600646836f55fa6512
Emma Davenport <Davenport.Physics@gmail.com>
2018-11-14 19:35:06 -0600

    Script changes
    Former-commit-id: fd10b8fb5c23c738468211f92116b8819f5c0972

---
+++ design/mindmaps/Days/Day 2.mm
@@ -0,0 +1,84 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Day 2" FOLDED="false" ID="ID_817968939" CREATED="1542229744746" MODIFIED="1542231330529" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="4" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Wakes up at home" POSITION="right" ID="ID_51147451" CREATED="1542229813597" MODIFIED="1542231330528">
+<edge COLOR="#ff0000"/>
+<node TEXT="Notices his mom isn&apos;t home" ID="ID_1790248128" CREATED="1542229887773" MODIFIED="1542231148767">
+<node TEXT="Goes to find mom" ID="ID_1218248520" CREATED="1542230170741" MODIFIED="1542231148766" HGAP_QUANTITY="115.9999969601632 pt" VSHIFT_QUANTITY="1.4999999552965178 pt"/>
+</node>
+<node TEXT="Windows are broken" ID="ID_1638070206" CREATED="1542230233959" MODIFIED="1542230240499"/>
+<node TEXT="Furniture is missing" ID="ID_683193034" CREATED="1542230240997" MODIFIED="1542230250196"/>
+</node>
+<node TEXT="Wakes up outside" POSITION="right" ID="ID_1192484302" CREATED="1542229834632" MODIFIED="1542231326242" HGAP_QUANTITY="9.50000013411045 pt" VSHIFT_QUANTITY="14.999999552965175 pt">
+<edge COLOR="#0000ff"/>
+<node TEXT="World is different" ID="ID_715106978" CREATED="1542230189144" MODIFIED="1542230317612" HGAP_QUANTITY="11.750000067055224 pt" VSHIFT_QUANTITY="1.4999999552965178 pt">
+<node TEXT="Goes to find mom" ID="ID_971340846" CREATED="1542230203756" MODIFIED="1542230317612" HGAP_QUANTITY="175.24999519437566 pt" VSHIFT_QUANTITY="-0.7499999776482589 pt"/>
+</node>
+<node TEXT="Windows are broken and fires can be seen inside" ID="ID_1942090747" CREATED="1542230271013" MODIFIED="1542230310967"/>
+<node TEXT="Bones are scattered everywhere" ID="ID_953877970" CREATED="1542230312054" MODIFIED="1542230400423"/>
+</node>
+</node>
+</map>
