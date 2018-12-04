2eb5aaa5e8ade77926ed8a74337f9cf9e50a7075
Emma Davenport <Davenport.Physics@gmail.com>
2018-12-04 16:57:12 -0600

    Slight optimization
    Former-commit-id: 865485f489d2ca28c3bbd7dc351c04fd9d5ac36e

--- src/collision/collision.lua
+++ src/collision/collision.lua
@@ -12,6 +12,8 @@ function Collision:new(x_pos, y_pos, width, height, name)
     obj.y_pos  = y_pos
     obj.width  = width
     obj.height = height
+    obj.end_x  = x_pos + width
+    obj.end_y  = y_pos + height
     obj.name   = name or ""
 
     return obj
@@ -26,11 +28,11 @@ end
 
 function Collision:CheckForCollision(x, y)
 
-    if not Shared.IsBetweenRange(x, self.x_pos, self.x_pos + self.width) then
+    if not Shared.IsBetweenRange(x, self.x_pos, self.end_x) then
         return false
     end
 
-    if not Shared.IsBetweenRange(y, self.y_pos, self.y_pos + self.height) then
+    if not Shared.IsBetweenRange(y, self.y_pos, self.end_y) then
         return false
     end
 
@@ -40,10 +42,10 @@ end
 
 function Collision:CheckForCollisionAdvanced(x, y, width, height)
 
-    if (x + width < self.x_pos or x > self.x_pos + self.width) then
+    if (x > self.end_x or x + width < self.x_pos) then
         return false
     end
-    if (y + height < self.y_pos or y > self.y_pos + self.height) then
+    if (y > self.end_y or y + height < self.y_pos) then
         return false
     end
 
