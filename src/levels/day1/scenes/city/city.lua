7199591e42e05a0229b9e9260f3318890b19af95
Emma Davenport <Davenport.Physics@gmail.com>
2018-12-12 15:39:01 -0600

    Polished maps
    Former-commit-id: e90695ac46e82a420f6872959ddde1ed55629a97

--- src/levels/day1/scenes/city/city.lua
+++ src/levels/day1/scenes/city/city.lua
@@ -347,12 +347,12 @@ end
 
 local DoNotGoInText =
 {
-    {x_pos = 23*16, y_pos = 59*16, text = TextBubbleClass:newThought(Henry, "Locked...")},
+    --{x_pos = 23*16, y_pos = 59*16, text = TextBubbleClass:newThought(Henry, "Locked...")},
     {x_pos = 29*16, y_pos = 59*16, text = TextBubbleClass:newThought(Henry, "Locked...")},
     {x_pos = 35*16, y_pos = 59*16, text = TextBubbleClass:newThought(Henry, "Locked...")},
     {x_pos = 16*16, y_pos = 40*16, text = TextBubbleClass:newThought(Henry, "I shouldn't go here")},
     {x_pos = 25*16, y_pos = 41*16, text = TextBubbleClass:newThought(Henry, "I shouldn't go here")},
-    {x_pos = 53*16, y_pos = 37*16, text = TextBubbleClass:newThought(Henry, "...")},
+    --{x_pos = 53*16, y_pos = 37*16, text = TextBubbleClass:newThought(Henry, "...")},
     {x_pos = 36*16, y_pos = 41*16, text = TextBubbleClass:newThought(Henry, "I shouldn't go here")},
     {x_pos = 67*16, y_pos = 8*16, text = TextBubbleClass:newThought( Henry, "I shouldn't go here")},
     {x_pos = 74*16, y_pos = 6*16, text = TextBubbleClass:newThought( Henry, "Nothing here..."), range = 48},
